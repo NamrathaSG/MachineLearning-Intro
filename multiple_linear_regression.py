9e8494bd1a8bd502338f24f06b77cbcc985b2792
Namratha S <50361336+NamrathaSG@users.noreply.github.com>
2020-12-21 12:44:14 +0530

    Update multiple_linear_regression.py

--- multiple_linear_regression.py
+++ multiple_linear_regression.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('50_Startups.csv')
+dataset = pd.read_csv('Enter_the_name_of_your_file.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, -1].values
 print(X)
