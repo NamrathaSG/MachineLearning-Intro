b0c1cb89439792894f92c7028b37c773c26c60a6
Namratha S <50361336+NamrathaSG@users.noreply.github.com>
2020-12-21 12:45:39 +0530

    Update polynomial_regression.py

--- polynomial_regression.py
+++ polynomial_regression.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 
 # Importing the dataset
-dataset = pd.read_csv('Position_Salaries.csv')
+dataset = pd.read_csv('Enter_the_name_of_your_file.csv')
 X = dataset.iloc[:, 1:-1].values
 y = dataset.iloc[:, -1].values
 
@@ -25,17 +25,17 @@ lin_reg_2.fit(X_poly, y)
 # Visualising the Linear Regression results
 plt.scatter(X, y, color = 'red')
 plt.plot(X, lin_reg.predict(X), color = 'blue')
-plt.title('Truth or Bluff (Linear Regression)')
-plt.xlabel('Position Level')
-plt.ylabel('Salary')
+plt.title('x vs y (Linear Regression)')
+plt.xlabel('x')
+plt.ylabel('y')
 plt.show()
 
 # Visualising the Polynomial Regression results
 plt.scatter(X, y, color = 'red')
 plt.plot(X, lin_reg_2.predict(poly_reg.fit_transform(X)), color = 'blue')
-plt.title('Truth or Bluff (Polynomial Regression)')
-plt.xlabel('Position level')
-plt.ylabel('Salary')
+plt.title('x vs y (Polynomial Regression)')
+plt.xlabel('x')
+plt.ylabel('y')
 plt.show()
 
 # Visualising the Polynomial Regression results (for higher resolution and smoother curve)
@@ -43,9 +43,9 @@ X_grid = np.arange(min(X), max(X), 0.1)
 X_grid = X_grid.reshape((len(X_grid), 1))
 plt.scatter(X, y, color = 'red')
 plt.plot(X_grid, lin_reg_2.predict(poly_reg.fit_transform(X_grid)), color = 'blue')
-plt.title('Truth or Bluff (Polynomial Regression)')
-plt.xlabel('Position level')
-plt.ylabel('Salary')
+plt.title('x vs y (Polynomial Regression)')
+plt.xlabel('x')
+plt.ylabel('y')
 plt.show()
 
 # Predicting a new result with Linear Regression
